4bf7c287d4d5ebc86bd29417d38a340e3239db8d
ernesthenry <henry38ernest@gmail.com>
2020-03-27 00:04:59 +0300

    Create a 404 page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import HomePage from "./pages/Homepage"
 import AboutPage from './pages/AboutPage'
 import ArticlesListPage from './pages/ArticlesListPage'
 import ArticlePage from './pages/ArticlePage'
+import NotFoundPage from './pages/NotFoundPage'
 import NavBar from './NavBar'
 
 import { 
   BrowserRouter as Router,
-  Route
+  Route,
+  Switch
 } from 'react-router-dom'
 import './App.css';
 
@@ -17,10 +19,14 @@ function App() {
       <div className="App">
         <NavBar />
         <div id="page-body">
-          <Route  path="/" component={HomePage} exact />
-          <Route  path="/about" component={AboutPage}/>
-          <Route  path="/articles-list" component={ArticlesListPage} />
-          <Route  path="/article/:name" component={ArticlePage} />
+          <Switch>
+            <Route  path="/" component={HomePage} exact />
+            <Route  path="/about" component={AboutPage}/>
+            <Route  path="/articles-list" component={ArticlesListPage} />
+            <Route  path="/article/:name" component={ArticlePage} />
+            <Route component={NotFoundPage}/>
+          </Switch>
+        
         </div>
        
       </div>
--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import articles from './article-content'
+import NotFoundPage from './NotFoundPage'
 import ArticlesList from '../components/ArticlesLIst'
 
 const ArticlePage = ({ match }) => {
     const name = match.params.name
     const article = articles.find((article) => article.name===name)
-    if(!article) return <h1>Article does not exist</h1>
+
+    if(!article) return <NotFoundPage />
     const otherArticles = articles.filter((article) => article.name !== name)
     return (
     <>
