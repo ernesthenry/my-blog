1a4a56be753cf106d4ef8d55fc116ec6693bdbd6
ernesthenry <henry38ernest@gmail.com>
2020-03-31 20:50:09 +0300

    Upvoting articles

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -20,8 +20,11 @@ const app = express()
 
 app.use(bodyParser.json())
 
-app.get('/hello', (req, res) => res.send('Hello'))
-app.get('/hello/:name', (req, res) => res.send(`Hello ${req.params.name}`))
-app.post('/hello', (req, res) => res.send(`Hello ${req.body.name}`))
+app.post('/api/articles/:name/upvote', (req, res) => {
+    const articleName = req.params.name;
+    articlesInfo[articleName].upvotes += 1;
+    res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`)
+
+})
 
 app.listen(8000, () => console.log('Listening on port 8000'))
